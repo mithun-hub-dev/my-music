ca9cbcd1d7094a9a9f051c271567e034075b519a
mithun-hub-dev <youcandoitbuddy@gmail.com>
2026-07-21 04:00:52 +0530

    Update songs.js

--- songs.js
+++ songs.js
@@ -1,1 +1,23 @@
+// ==========================
+// Mithun's Music List
+// ==========================
 
+const playlist = [
+
+{
+title:"Enthara Enthara",
+artist:"Tamil OST",
+movie:"Thirumanam Enum Nikkah",
+file:"songs/Enthara-Enthara.mp3",
+cover:"images/Thirumanam_ennum_nikkah.jpg"
+},
+
+{
+title:"Gulmohar Malare",
+artist:"Tamil OST",
+movie:"Majunu",
+file:"songs/Gulmohar-Malare.mp3",
+cover:"images/Majunu.jpg"
+}
+
+];
